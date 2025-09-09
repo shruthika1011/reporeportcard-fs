e09ddffd1b4c51177e6d653bd967755d95146ff2
Shruthika <shruthikapadma2004@gmail.com>
2025-09-09 23:30:06 +0530

    hgfd

--- backend/reportcard-backend/src/main/java/com/reportcard/backend/ReportCardBackendApplication.java
+++ backend/reportcard-backend/src/main/java/com/reportcard/backend/ReportCardBackendApplication.java
@@ -2,9 +2,18 @@ package com.reportcard.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ReportCardBackendApplication {
+public class ReportCardBackendApplication extends SpringBootServletInitializer {
+
+    // This is required for WAR deployment in external Tomcat
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ReportCardBackendApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ReportCardBackendApplication.class, args);
     }
